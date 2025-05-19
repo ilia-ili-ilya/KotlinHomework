ebc7260dc6c248983301269648efa16adf6346f7
Ilya Ivanov <ilya@MacBook-Pro-Ilya.local>
2025-05-19 17:05:43 +0300

    add build gradle

--- third_task/build.gradle.kts
+++ third_task/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     kotlin("plugin.serialization") version "2.0.21"
 }
 
-group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -14,6 +13,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("org.knowm.xchart:xchart:3.8.0")
     testImplementation(kotlin("test"))
+    implementation("space.kscience:kmath-core:0.3.1")
+    implementation("org.apache.commons:commons-math3:3.6.1")
 }
 
 tasks.test {
